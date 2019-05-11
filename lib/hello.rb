6189922f7f7bd51cac71172d7e9f43358f6e246f
Jasmine Chen <jasmine1226@gmail.com>
2019-05-11 22:32:04 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,15 +5,16 @@ def hello_t(array)
     yield array[i]
     i+=1
   end
-  if array = []
-    puts "Hey! No block was given!\n"
-  end
+
     return array
 end
 
 # call your method here!
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if array = []
+    puts "Hey! No block was given!\n"
+  end
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
